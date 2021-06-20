5d2d8a47ccd1593ca70400663659de2f847a8b7d
zainsci <60652827+zainsci@users.noreply.github.com>
2021-06-20 20:24:18 +0500

    feat: add Hiragana & Katakana Page

---
+++ src/pages/katakana/index.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react"
+
+import Layout from "../../components/layout"
+
+const Katakana = () => {
+  const [chars, setChars] = useState([])
+  const charGrid = []
+
+  useEffect(() => {
+    fetch("/assets/katakana.json")
+      .then((res) => res.json())
+      .then((data) => setChars(data))
+  }, [])
+
+  chars.forEach((char) => {
+    charGrid.push(
+      char.type ? (
+        <div className="bg-indigo-500 text-white flex flex-col justify-center items-center px-6 py-8 font-display">
+          <div className="text-8xl mb-2">{char.char}</div>
+          <small>{char.romaji}</small>
+        </div>
+      ) : (
+        <div className="bg-indigo-200"></div>
+      )
+    )
+  })
+
+  return (
+    <Layout>
+      <div className="grid grid-cols-5 gap-4">{charGrid}</div>
+    </Layout>
+  )
+}
+
+export default Katakana
