5afc589d2f8cb89d6e5969a593f35e6c36f72343
zainsci <60652827+zainsci@users.noreply.github.com>
2021-06-25 19:17:38 +0500

    style: style kana page

--- src/pages/[syllabary]/[id].js
+++ src/pages/[syllabary]/[id].js
@@ -18,7 +18,23 @@ const HiraganaChar = () => {
 
   return (
     <Layout>
-      <div>{kana.char}</div>
+      <div className="grid grid-cols-3 gap-10 w-full min-h-full">
+        <div
+          className="text-transparent bg-gradient-to-r from-green-600 to-indigo-500 bg-clip-text flex justify-center items-start"
+          style={{
+            fontSize: "14rem",
+          }}
+        >
+          {kana.kana}
+        </div>
+        <div className="flex flex-col items-start">
+          <h1 className="text-6xl">
+            <span className="font-bold">KANA: </span> {kana.kana}
+          </h1>
+          <h2 className="text-5xl">ROMAJI: {kana.romaji}</h2>
+        </div>
+        <div></div>
+      </div>
     </Layout>
   )
 }
