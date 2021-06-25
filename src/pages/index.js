1166dd854116380581950f84de498587f59a653b
zainsci <60652827+zainsci@users.noreply.github.com>
2021-06-25 20:01:14 +0500

    style: redesign main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,28 +6,37 @@ export default function Home() {
   return (
     <Layout>
       <div className="container -mt-20">
-        <div className="grid grid-cols-2">
-          <div
-            style={{ fontSize: "20rem" }}
-            className="flex justify-center items-center font-display bg-gradient-to-br from-green-500 to-purple-900 bg-clip-text text-transparent"
-          >
-            あ
+        <div className="grid grid-cols-2 gap-10">
+          <div className="px-20 py-20 w-full h-full bg-purple-600 overflow-hidden relative">
+            <span className="">
+              <h1 className="mb-6 text-left text-4xl font-bold text-white">
+                BENGO
+              </h1>
+              <p className="mb-4 text-left text-lg text-white">
+                Learn Hiragana and Katakana with ease using mnemonics in days
+                and not in months or years.
+              </p>
+            </span>
+            <span className="font-bold text-white text-8xl opacity-30 absolute -bottom-5 -right-5">
+              日本語
+            </span>
           </div>
-          <div className="flex flex-col justify-center items-start">
-            <h1 className="text-4xl mb-6">BENGO</h1>
-            <p className="text-lg mb-4 text-left">
-              Learn Hiragana and Katakana with ease using mnemonics in days
-              <br />
-              and not in months or years.
-            </p>
+          <div className="flex flex-col justify-start items-start">
+            <p className="font-bold text-2xl mb-6">Start Learning</p>
             <Link href="/hiragana">
-              <a className="px-6 py-2 mb-2 rounded-sm bg-purple-600 text-white uppercase font-bold border border-purple-600 hover:bg-purple-500 transition duration-300 drop-shadow-lg focus:outline-none">
-                Hiragana
+              <a className="px-12 py-8 mb-2 bg-green-700 border border-greebg-green-700 hover:bg-green-800 transition duration-300 drop-shadow-lg overflow-hidden">
+                <span className="text-white font-bold text-3xl">HIRAGANA</span>
+                <span className="font-bold text-5xl text-white absolute -bottom-3 -right-2 opacity-30">
+                  平仮名
+                </span>
               </a>
             </Link>
-            <Link href="/katakana">
-              <a className="px-6 py-2 mb-2 rounded-sm bg-white text-purple-600 uppercase font-bold border border-purple-600 hover:bg-purple-100 hover:border-purple-700 transition duration-300 drop-shadow-lg focus:outline-none">
-                Katakana
+            <Link href="/hiragana">
+              <a className="px-12 py-8 mb-2 bg-indigo-600 border border-greebg-indigo-600 hover:bg-indigo-700 transition duration-300 drop-shadow-lg overflow-hidden">
+                <span className="text-white font-bold text-3xl">KATAKANA</span>
+                <span className="font-bold text-5xl text-white absolute -bottom-3 -right-2 opacity-30">
+                  片仮名
+                </span>
               </a>
             </Link>
           </div>
